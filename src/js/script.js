5f287df1a142c34471c4aa851addcd725f6930e3
Gabriel <gabrielviana1939@icloud.com>
2025-03-28 11:43:37 -0300

    declaração de variaveis

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+// declaração e variaveis
+
+var nome ="Fiap";
+console.log(nome)
+
+let idade =19;
+console.log(idade)
+
+const sobrenome ="fiapinho"
+console.log(sobrenome)
+
+//underined
+
+let exemplo;
+console.log(exemplo)
+
+//nula
+
+let exemolo1 =null;
+console.log(exemplo)
