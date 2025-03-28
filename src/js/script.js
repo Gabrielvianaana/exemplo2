807156e2598156ccee94c362c4c8e2a87ebab3a8
Gabriel <gabrielviana1939@icloud.com>
2025-03-28 11:52:21 -0300

    criando tipos de vaiaveis

--- src/js/script.js
+++ src/js/script.js
@@ -15,6 +15,19 @@ let exemplo;
 console.log(exemplo)
 
 //nula
+let ex1 = "fiap";
+console.log(typeof ex1)
 
-let exemolo1 =null;
-console.log(exemplo)
+let ex2 = 25;
+console.log(typeof ex2)
+
+let ex3 = true;
+console.log (typeof ex3)
+
+let ex4 = {}
+console.log (typeof ex4)
+
+    let ex5 = []
+    console.log(typeof ex5)
+
+
